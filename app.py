edc22aa781267c8513e8e1be30b0cd2a54dab2ad
Thomas_vadakkan <32524248+thomas-vadakkan@users.noreply.github.com>
2021-08-14 22:32:59 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import numpy as np
 thres = 0.5
 nms_threshold = 0.2
 # cap = cv2.VideoCapture('https://192.168.1.6:8080/video')
-cap = cv2.VideoCapture('https://www.cameraftp.com/cameraftp/publish/camera.aspx/parentID75626517/shareID13284794/modelive/nameporch/camTypeCamera')
+cap = cv2.VideoCapture('rtsp://192.168.0.160/video1')
 # cap = cv2.imread('image01.jpg')
 cap.set(3, 640)  # width
 cap.set(4, 480)  # height
